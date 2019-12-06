89f1a489cf217fd1cbb082ba1e0794e94fb6aad8
Roddy McNeill <roddy@192-168-1-23.tpgi.com.au>
2019-12-06 13:19:51 +1100

    Done stuff

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DirectoryList from '../../components/directory-list/directory-list.component';
+
+import "./homepage.styles.scss";
+
+const Homepage = () => (
+    <div className="homepage">
+        <DirectoryList />
+    </div>
+)
+
+export default Homepage;
